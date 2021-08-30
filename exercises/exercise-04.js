7906635fd34cf30331a416458b81c7813ede92af
daliayan <daliayan5@gmail.com>
2021-08-30 19:27:34 -0400

    Connecting

--- exercises/exercise-04.js
+++ exercises/exercise-04.js
@@ -1,2 +1,3 @@
 // Write a function using the movieList array and sort the movies by their release date, starting with the most recent release date. Return the sorted array.
 // A movie with a release date of 2015 should come before a movie with a release date of 2010. 
+sdsd
