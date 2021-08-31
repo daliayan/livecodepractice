75b7e751570650167a0e5171175b9014aa81ffbe
daliayan <daliayan5@gmail.com>
2021-08-31 16:32:04 -0400

    Completed exercise 4

--- exercises/exercise-04.js
+++ exercises/exercise-04.js
@@ -1,3 +1,26 @@
 // Write a function using the movieList array and sort the movies by their release date, starting with the most recent release date. Return the sorted array.
 // A movie with a release date of 2015 should come before a movie with a release date of 2010. 
-sdsd
+
+// let order = (x >= )
+
+// const movieList = []
+// movieList.sort()
+
+const organizedMovie = movieList.sort()
+
+
+organizedMovie.sort(function(a,b) {
+    return (a.year - b.year) || (monthsArray/indexOf(a.month) - monthsArray.indexOf(b.month)); 
+})
+console.log(organizedMovie);
+
+
+
+// console.log(organizedMovie);
+
+// function compareMovie(movieList){
+//     if movieList.release_date >= 2015
+
+// };
+
+// console.log(movieList)
